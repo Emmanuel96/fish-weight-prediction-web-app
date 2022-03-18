bd7013aef6782a0cc8b733256c3aabd6af346f5e
emmanuelaudu <emmanuel.audu1@aun.edu.ng>
2022-03-18 04:58:33 -0230

    very first commit with all initial files

---
+++ app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 08:46:21 2022
+
+@author: emmanuelaudu
+"""
+
+import pickle
+import numpy as np
+import pandas as pd
+from flask import Flask, request
+from flask import Flask, render_template
+import sys
+
+app=Flask(__name__)
+
+pickle_in = open("model.pkl",'rb')
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def home():
+    return render_template("model_test.html")
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [x for x in request.form.getlist('data[]')]
+    # print(request.form.getlist('data[]'), flush = True)
+    final_features = [np.array(int_features)]
+
+    prediction = classifier.predict(final_features)
+
+    prediction_text='The weight of the fish is {}'.format(prediction)
+
+    return prediction_text
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
