049735e731f30ffa99c70c4f642f94fe89240154
emmanuelaudu <emmanuel.audu1@aun.edu.ng>
2022-04-20 22:39:45 -0230

    make it better

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import Flask, render_template
 import sys
 
 app=Flask(__name__)
-
+ 
 pickle_in = open("model.pkl",'rb')
 classifier=pickle.load(pickle_in)
 
@@ -29,12 +29,13 @@ def predict():
     For rendering results on HTML GUI
     '''
     int_features = [x for x in request.form.getlist('data[]')]
-    # print(request.form.getlist('data[]'), flush = True)
+    print(request.form.getlist('data[]'), flush = True)
+    
     final_features = [np.array(int_features)]
 
     prediction = classifier.predict(final_features)
 
-    prediction_text='The weight of the fish is {}'.format(prediction)
+    prediction_text='The close price of bitcoin on this day will be {}'.format(prediction)
 
     return prediction_text
 
